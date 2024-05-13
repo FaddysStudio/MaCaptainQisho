754a54af6e84fb05e1c6f9e632972d1d7b06f795
Faddy Michel Samaan <faddy.michel@gmail.com>
2024-05-13 18:53:23 -0400

    @faddymichel: On branch main
    *	deleted:    index.js
    *	modified:   index.rs

--- beat/index.rs
+++ beat/index.rs
@@ -1,6 +1,53 @@
 $ rm -rf .FaddysBeat ; mkdir -p .FaddysBeat ; ln -s ~/studio/kit/percussive/*.wav .FaddysBeat
 
-$ node . 0/dom 1/tak 3/tak 4/dom 6/tak > .FaddysBeat/maqsum.sco
+$ cat - > .FaddysBeat/index.js
+
+import Scenarist from '@faddys/scenarist';
+
+await Scenarist ( new class Beat {
+
+instrument = 13
+tempo = 110
+length = 4
+measure = 8
+sequence = []
+
+async $_producer ( $ ) {
+
+await $ ( ... process .argv .slice ( 2 ) );
+
+const beat = this;
+
+console .log ( `
+
+t 0 ${ beat .tempo }
+v ${ beat .length }
+
+${ beat .sequence .join ( '\n' ) }
+
+s ${ beat .length }
+
+` .trim () );
+
+}
+
+async $_director ( $, ... argv ) {
+
+if ( ! argv .length )
+return '';
+
+const beat = this;
+const [ step, sample ] = argv .shift () .split ( '/' );
+
+beat .sequence .push ( `i ${ beat .instrument }.${ beat .sequence .length } [${ step }/${ beat .measure }] 1 ".FaddysBeat/${ sample }.wav"` );
+
+await $ ( ... argv );
+
+}
+
+} );
+
+$ node .FaddysBeat 0/dom 1/tak 3/tak 4/dom 6/tak > .FaddysBeat/maqsum.sco
 
 $ cat - > .FaddysBeat/maqsum.orc
 
